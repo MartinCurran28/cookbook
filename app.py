a27a55c99425123b327f7039e8e21a9767c42b5e
Martin Curran <martincurran28@hotmail.com>
2018-12-09 14:42:13 +0000

    Added an addrecipe.html view and route

--- app.py
+++ app.py
@@ -1,17 +1,25 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 
 app = Flask(__name__)
 
 app.config["MONGO_DBNAME"] = 'myveryowncookbook'
-app.config["MONGO_URI"] = 'mongodb://<dbuser>:<dbpassword>@ds227654.mlab.com:27654/myveryowncookbook'
+app.config["MONGO_URI"] = 'mongodb://admin:Vonnegut28@ds227654.mlab.com:27654/myveryowncookbook'
 
 mongo = PyMongo(app)
 
 @app.route('/')
-def hello_world():
-    return "Hello World! Here's my cookbook!"
+
+    
+@app.route('/add_recipe')
+def add_recipe():
+    return render_template("addrecipe.html")
+    
+@app.route('/get_recipes')
+def get_recipes():
+    return render_template("recipes.html", 
+    recipes=mongo.db.recipes.find())    
     
 if __name__ == "__main__":
     app.run(host=os.environ.get('IP'),
