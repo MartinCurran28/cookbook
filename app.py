517479cb3c46fa693bbf69856b653252b05b5420
Martin Curran <martincurran28@hotmail.com>
2018-12-09 15:27:14 +0000

    Used ginja in the recipe view to route recipe data to the browser.

--- app.py
+++ app.py
@@ -10,16 +10,12 @@ app.config["MONGO_URI"] = 'mongodb://admin:Vonnegut28@ds227654.mlab.com:27654/my
 mongo = PyMongo(app)
 
 @app.route('/')
-
-    
-@app.route('/add_recipe')
-def add_recipe():
-    return render_template("addrecipe.html")
-    
 @app.route('/get_recipes')
 def get_recipes():
-    return render_template("recipes.html", 
-    recipes=mongo.db.recipes.find())    
+    return render_template("recipes.html",
+    recipes=mongo.db.recipes.find())
+    
+    
     
 if __name__ == "__main__":
     app.run(host=os.environ.get('IP'),
