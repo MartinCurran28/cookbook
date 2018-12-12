0b4fd7a699a1dcef0fd2fdfbedb43ba288a8ae10
Martin Curran <martincurran28@hotmail.com>
2018-12-12 19:03:48 +0000

    Introduced a simple user registration form to save user data to a new MongoDB collection

--- app.py
+++ app.py
@@ -10,16 +10,25 @@ app.config["MONGO_URI"] = 'mongodb://admin:Vonnegut28@ds227654.mlab.com:27654/my
 mongo = PyMongo(app)
 
 @app.route('/')
-@app.route('/get_recipes')
-def get_recipes():
-    return render_template("recipes.html", 
-    recipes=mongo.db.recipes.find())
+
+@app.route('/add_user')
+def add_user():
+    return render_template("user_registration.html")
+
+@app.route('/insert_user', methods=['POST'])
+def insert_user():
+    users = mongo.db.users
+    users.insert_one(request.form.to_dict())
+    return redirect(url_for("entry"))
+    
+@app.route('/entry')
+def entry():
+    return render_template("entry.html")
 
 @app.route('/add_recipe')
 def add_recipe():
     return render_template('addrecipe.html',
     categories=mongo.db.categories.find())
-    
     
 @app.route('/insert_recipe', methods=['POST'])
 def insert_recipe():
@@ -27,7 +36,10 @@ def insert_recipe():
     recipes.insert_one(request.form.to_dict())
     return redirect(url_for('get_recipes'))
     
-
+@app.route('/get_recipes')
+def get_recipes():
+    return render_template("recipes.html", 
+    recipes=mongo.db.recipes.find())
     
 if __name__ == "__main__":
     app.run(host=os.environ.get('IP'),
